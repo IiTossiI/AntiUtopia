07eaccada9df8599d84cec8356c623622c5f0be4
Toss <94807008+IiTossiI@users.noreply.github.com>
2023-09-09 19:57:43 +0300

    Updated main.js

--- main.js
+++ main.js
@@ -4,6 +4,9 @@ fetch("https://antiutopia.glitch.me/latest.news").then(response => {
     if (response.status == 200) {
         response.text().then(data => {
             data = data.split("\n")
+            if (typeof data[4] == "undefined") {
+        	    data[4] = "no-image.png"
+            }
             news.innerHTML = '<h1>' + data[0] + '</h1><h6>' + data[1] + '</h6>' + data[3] + '<h6>' + data[2] + '</h6><img src="' + data[4] + '">'
         })
     } else {
